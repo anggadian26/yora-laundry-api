2dd61d00b1a7d6eb9ab30cda9e7adfb25735234f
Angga Dian <anggadiansap@gmail.com>
2026-07-22 20:38:06 +0700

    YRA-212 getallcustomer

--- controller/customers.controller.js
+++ controller/customers.controller.js
@@ -1,9 +1,20 @@
-const { Customer } = require('../models')
+const { Customers } = require('../models')
 const Validator = require('fastest-validator')
 const v = new Validator();
 
 const getAllCustomer = async (req, res) => {
-
+   try {
+      const data = await Customers.findAll({attributes: { exclude: ['createdAt'] }})
+      return res.status(200).json({
+         message: "Get All Customer",
+         data: data
+      })
+   } catch (err) {
+      return res.status(500).json({
+         message: 'Error Server',
+         error: err.message 
+      });
+   }
 }
 
 const getCustomerById = async (req, res) => {
